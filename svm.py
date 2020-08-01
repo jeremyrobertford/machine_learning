4934b1f38df9ae3475287897b3a7e8626626d53a
Jeremy <jeremyrobertford@gmail.com>
2020-08-01 00:45:04 -0400

    Finish svm model.

--- svm.py
+++ svm.py
@@ -1,19 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
-import pandas as pd
-
 style.use('ggplot')
 
-data_dict = {-1: np.array([[1, 7],
-                           [2, 8],
-                           [3, 8]]),
-              1: np.array([[5, 1],
-                           [6, -1],
-                           [7, 3]])
-            }
-class SVM:
 
+class Support_Vector_Machine:
     def __init__(self, visualization=True):
         self.visualization = visualization
         self.colors = {1: 'r', -1: 'b'}
@@ -23,73 +14,140 @@ class SVM:
 
     def fit(self, data):
         self.data = data
-        # { ||w|| = [w, b] }
-        opt_dict = {}
+        optimum_dict = {}
 
-        transforms = [[ 1, 1],
+        transforms = [[1, 1],
                       [-1, 1],
-                      [-1,-1],
-                      [ 1,-1]]
+                      [-1, -1],
+                      [1, -1]]
 
-        all_data = []
+        self.max_feature_value = max([np.max(self.data[x]) for x in self.data])
+        self.min_feature_value = min([np.min(self.data[x]) for x in self.data])
 
-        for yi in self.data:
-            for features in self.data[yi]:
-                for feature in features:
-                    all_data.append(feature)
+        # support vectors yi(xi.w+b) = 1
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      # point of expense:
+                      self.max_feature_value * 0.001, ]
 
-        self.max = max(all_data)
-        self.min = min(all_data)
-        del all_data
-
-        step_sizes = [self.max * 0.1,
-                      self.max * 0.01,
-                      self.max * 0.001
-                     ]
-
-        # extremely expensive to fine tune
-        b_mult = 5
-
-        opt = self.max * 10
+        # extremely expensive
+        b_multiple = 5
+        latest_optimum = self.max_feature_value * 10
 
         for step in step_sizes:
-            w = np.array([opt, opt])
+            w = np.array([latest_optimum, latest_optimum])
+            # we can do this because convex
             optimized = False
             while not optimized:
-                r = range(-1 * self.max * b_mult, self.max * b_mult, b_mult)
-                for b in r:
+                data_range = self.max_feature_value * b_multiple
+                for b in np.arange(-1 * data_range,
+                                   data_range,
+                                   step * b_multiple):
                     for t in transforms:
                         w_t = w * t
-                        found = True
-                        # weakest link in the SVM -> iterating over 
-                        # the entire dataset
-                        # SMO attempts to fix this
+                        found_option = True
+                        # Expensive point of SVM model: cycling through
+                        # all the data.
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi = i
-                                if not yi * (np.dot(w_t,xi)+b) >= 1:
+                                # Check if point is support vector
+                                # yi(xi.w+b) >= 1
+                                if not yi*(np.dot(w_t, xi)+b) >= 1:
                                     found_option = False
-                                    break
+
                         if found_option:
-                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+                            # { ||w||: [w, b] }
+                            w_mag = np.linalg.norm(w_t)
+                            optimum_dict[w_mag] = [w_t, b]
 
                 if w[0] < 0:
                     optimized = True
                     print('Optimized a step.')
                 else:
                     w = w - step
-            norms = sorted(list(opt_dict.keys()))
-            opt_choice = opt_dict[norms[0]]
 
-            self.w = opt_choice[0]
-            self.b = opt_choice[1]
+            norms = sorted(optimum_dict.keys())
+            # ||w|| : [w, b]
+            optimum_choice = optimum_dict[norms[0]]
+            self.w = optimum_choice[0]
+            self.b = optimum_choice[1]
+            latest_optimum = optimum_choice[0][0] + step * 2
 
-            opt = opt_choice[0][0] + step * 2
+    def predict(self, features):
+        # sign(xi.w+b)
+        # c = classification
+        c = np.sign(np.dot(np.array(features), self.w)+self.b)
 
-    def predict(self, data):
-        c = np.sign(np.dot(np.array(data), self.w) + self.b)
+        if c != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1],
+                            s=200, marker='*', c=self.colors[c])
+        else:
+            print('featureset', features, 'is on the decision boundary')
+
         return c
 
+    def visualize(self):
+        for i in data_dict:
+            for x in data_dict[i]:
+                self.ax.scatter(x[0], x[1], s=100, color=self.colors[i])
 
-if __name__='__main__':
-    pass
+        # hyperplane = x.w+b
+        # v = x.w+b
+        # psv = 1
+        # nsv = -1
+        # dec = 0
+        def hyperplane(x, w, b, v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # Create points for hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        # Create line
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
+
+        # (w.x+b) = -1
+        # Create points for hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        # Create line
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
+
+        # (w.x+b) = 0
+        # Create points for hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        # Create line
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    data_dict = {-1: np.array([[1, 7],
+                              [2, 8],
+                              [3, 8], ]),
+
+                 1: np.array([[5, 1],
+                             [6, -1],
+                             [7, 3], ])}
+
+    svm = Support_Vector_Machine()
+    svm.fit(data=data_dict)
+    svm.visualize()
+    new_data = [[0, 10],
+                [1, 3],
+                [3, 4],
+                [3, 5],
+                [5, 5],
+                [5, 6],
+                [6, -5],
+                [5, 8]]
+
+for p in predict_us:
+    print(p, svm.predict(p))
